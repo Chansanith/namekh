44b3e721bd7f08ef8be5310776a497b8e5d8505d
Chansanith <33217719+Chansanith@users.noreply.github.com>
2023-07-13 22:52:46 +0700

    Create infogen.js

---
+++ infogen.js
@@ -0,0 +1,213 @@
+// Note: Text that comes after "//" is ignored, which allows you to write notes (like the one you're reading now!)
+
+title
+  Your Generator's Title
+
+output
+  <b>Category:</b> [animal] Pokémon <br> <b>Type:</b> [type1]/[type2] <br> <b>Evolution:</b> [evolution] <br> <b>Size:</b> [size] <br> <b>Shape:</b> [shape]
+	
+type1
+  Electric
+  Water
+  Fire
+  Grass
+  Ice
+  Ground
+  Rock
+  Dragon
+  Fairy
+  Steel
+  Bug
+  Fighting
+  Flying
+  Poison
+  Psychic
+  
+type2
+  Electric
+  Water
+  Fire
+  Grass
+  Ice
+  Ground
+  Rock
+  Dragon
+  Fairy
+  Steel
+  Bug
+  Fighting
+  Flying
+  Poison
+  Psychic
+  
+animal
+  Mouse
+  Rat
+  Dog
+  Cat
+  Horse
+  Zebra
+  Bear
+  Wombat
+  Bat
+  Marsupial
+  Kangaroo
+  Dolphin
+  Goo
+  Frog
+  Gecko
+  Lizard
+  Alligator
+  Candle
+  Box
+  Umbrella
+  Banana
+  Hedgehog
+  Kappa
+  Spider
+  Scorpion
+  Deer
+  Badger
+  Mink
+  Numbat
+  Dinosaur
+  Snowman
+  Skeletal
+  Cloud
+  Mannequin
+  Artist
+  Baby
+  Orb
+  Ominous
+  Travelling
+  Nightmare
+  Dream
+  Gazelle
+  Antelope
+  Elephant
+  Tapir
+  Hamster
+  Gerbil
+  Snake
+  Toad
+  Leopard
+  Hyena
+  Meerkat
+  Rabbit
+  Jackal
+  Chincilla
+  Koala
+  Coatimundi
+  Ape
+  Chicken
+  Lynx
+  Lion
+  Camel
+  Doll
+  Wolf
+  Salamander
+  Newt
+  Dugong
+  Squirrel
+  Blanket
+  UFO
+  Star
+  Moon
+  Sun
+  Ball
+  Balloon
+  Antennae
+  Turtle
+  Fox
+  Flower
+  Pig
+  Jellyfish
+  Horn
+  Trio
+  Bivalve
+  Lonely
+  Dancing
+  Transport
+  Virtual
+  Spiral
+  Helmet
+  Long
+  Goliath
+  Tiny
+  Royal
+  Shark
+  Glove
+  Shoe
+  Plump
+  Forbidden
+  Tree
+  Otter
+  Trash
+  Jewel
+  Punk
+  Baby
+  Twin
+  
+evolution
+  No Evolution
+  One Evolution
+  Two Evolutions
+  Three Evolutions
+  Item Evolution
+  Trade Evolution
+  High Friendship
+  Level Up At Night
+  Female Only Evolution
+  Male Only Evolution
+  
+size
+  0'00" - 0'08"
+  1'00" - 1'08"
+  2'00" - 2'11"
+  3'00" - 3'11"
+  4'00" - 4'11"
+  5'00" - 5'11"
+  6'00" - 6'11"
+  7'00" - 7'11"
+  8'00" - 8'11"
+  9'00" - 9'11"
+  10'00" - 10'11" ^0.5
+  11'00" - 11'11" ^0.5
+  12'00" - 12'11" ^0.4
+  13'00" - 13'11" ^0.3
+  14'00" - 14'11" ^0.2
+  15'00" - 15'11" ^0.1
+  
+shape
+  Only A Head
+  Head And Arms
+  Fins
+  Insectoid Body
+  Head And Legs
+  Quadruped
+  Two Or More Pairs Of Wings
+  Multiple Bodies (Dugtrio, Exeggcute, Magneton etc.)
+  Tentacles Or Multiple Limbs
+  Head And Body
+  Bipedal, With A Tail
+  Bipedal, Without A Tail
+  Single Pair Of Wings
+  Serpent Body
+
+ 
+// Tips:
+//  - You can change the URL of your generator by clicking the "settings" button in the top-right
+//  - Use this template if you want your generator to be vertically centered on the page:  perchance.org/centered-minimal#edit
+//  - Add background images, fonts, and other stuff with plugins: perchance.org/plugins
+//  - Here are some templates that you can use to create nice-looking generators without learning HTML and CSS:  perchance.org/templates
+//  - Use the "layout maker" plugin to create complex layouts without learning how to code:  perchance.org/layout-maker-plugin
+//  - Here are some generators you might like to import:  perchance.org/useful-generators
+//  - Highlight multiple lines and press Tab or Shift+Tab to indent and un-indent them all an once
+//  - Read this page after reading the tutorial:  perchance.org/examples
+//  - There's a "wrap" button in the top-right if your lines are really long. If you add   $output=[this.joinItems(" ")]   as the first item in your list, then writing [yourListName] will magically output all of the items joined together, rather than a random item from the "yourListName" list.
+//  - Ask our friendly community members if you need help:  reddit.com/r/perchance
+// \( ﾟヮﾟ)/
+
+
+
+
