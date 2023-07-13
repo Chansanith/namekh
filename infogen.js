059746d0f349bda867228e09eb4baef3624c3b83
Chansanith <33217719+Chansanith@users.noreply.github.com>
2023-07-13 23:06:42 +0700

    Update infogen.js

--- infogen.js
+++ infogen.js
@@ -1,43 +1,27 @@
-var group = [
-  "Benjamin Gouget",
-  "Florian Farris",
-  "Said Zenafi",
-  "Yannis Aouachria",
-  "Pierre Denaes",
-  "Morgan Trainoir",
-  "Coralie Damery",
-  "Quentin Robert",
-  "Alexandre Chauvet",
-  "Sebastien Cartoux",
-  "Davis Haas",
-  "Sofiane Sotehi",
-  "Yoan Baldacchino",
-  "Nicolas Degabriel",
-  "Ayman Bratzu",
-  "Quentin Queffuris",
-  "Killian Pasquier",
-  "Jérôme Laurent",
-  "Jean-Daniel Pontremoli",
-  "Marjorie Ngoupende",
-  "Christophe Coutures",
-  "Michel Christophe"
-];
+var NumberOfWords = 10
 
-const getRandomNumber = max => Math.floor(Math.random() * max);
-const getRandomName = () => `${group[getRandomNumber(group.length)]}`;
-const setRandomName = () => {
-  document.getElementById("random-name").innerText = getRandomName();
-};
+var words = new BuildArray(NumberOfWords)
 
-document.getElementById("generate").addEventListener("click", setRandomName);
-// action button complete list
-const fullList = () => {
-  document.getElementById("completList").innerHTML = "";
-  group.forEach(function(name) {
-    var p = document.createElement("p");
-    document.getElementById("completList").appendChild(p);
-    p.innerHTML += name;
-  });
-};
-document.getElementById("all-name").addEventListener("click", fullList);
+words[1] = "Guilty Crown"
+words[2] = "Overlord"
+words[3] = "Danganronpa"
+words[4] = "Needless"
+words[5] = "Heavy Object"
+words[6] = "Attack on Titan"
+words[7] = "Dragon Ball Super"
+words[8] = "Jojo's Bizarre Adventure"
+words[9] = "Persona 5"
+words[10] = "Jormungand"
 
+//thanks @Pete Hearne for the function!//
+function BuildArray(size){
+  this.length = size
+  for (var i = 1; i <= size; i++){
+    this[i] = null}
+  return this
+}
+
+function PickRandomWord(frm) {
+  var rnd = Math.ceil(Math.random() * NumberOfWords)
+  frm.wordspace.value = words[rnd]
+}
